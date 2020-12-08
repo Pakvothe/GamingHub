c55ffb3815d16fc14f6b169d259fb7de844041bf
NCDEV (PC ESCRITORIO) <nc.devw@gmail.com>
2020-12-08 13:05:54 -0300

    added api utilities-v1

---
+++ api/utils/categories.js
@@ -0,0 +1,42 @@
+const categories = [
+    {
+        name_en: 'FPS',
+        name_es: 'FPS',
+    },
+    {
+        name_en: 'Action',
+        name_es: 'Acción',
+    },
+    {
+        name_en: 'Simulation',
+        name_es: 'Simulacion',
+    },
+    {
+        name_en: 'Sports',
+        name_es: 'Deportes',
+    },
+    {
+        name_en: 'RPG',
+        name_es: 'RPG',
+    },
+    {
+        name_en: 'Strategy',
+        name_es: 'Estrategia',
+    },
+    {
+        name_en: 'Platformer',
+        name_es: 'Plataforma',
+    },
+    {
+        name_en: 'Adventure',
+        name_es: 'Aventura',
+    },
+    {
+        name_en: 'Arcade',
+        name_es: 'Arcade',
+    },
+    {
+        name_en: 'Racing',
+        name_es: 'Carreras',
+    },
+]
