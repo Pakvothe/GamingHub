57ffc4182d17ebccb2d671064b75ab6d519ba50e
Aglowkeys <emilianoalfonso@outlook.com>
2020-12-08 15:51:40 -0300

    Added Category Model v1

---
+++ api/src/models/Category.js
@@ -0,0 +1,27 @@
+const { DataTypes } = require('sequelize');
+const D = DataTypes;
+
+module.exports = (sequelize) => {
+    sequelize.define('category', {
+        name: {
+            type: D.STRING,
+            unique: true,
+            allowNull: false,
+            validate: {
+                isAlpha: true,
+                notEmpty: true,
+                len: [2, 25]
+            }
+        },
+        name_en: {
+            type: D.STRING,
+            unique: true,
+            allowNull: false,
+            validate: {
+                isAlpha: true,
+                notEmpty: true,
+                len: [2, 25]
+            }
+        }
+    })
+}
