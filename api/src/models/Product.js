4c58157b201e6357d89b28892b51c933632e0c46
Aglowkeys <emilianoalfonso@outlook.com>
2020-12-08 15:44:19 -0300

    Added product model - v1

--- api/src/models/Product.js
+++ api/src/models/Product.js
@@ -1,12 +1,41 @@
 const { DataTypes } = require('sequelize');
+const D = DataTypes;
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
 	// defino el modelo
 	sequelize.define('product', {
 		name: {
-			type: DataTypes.STRING,
+			type: D.STRING,
 			allowNull: false,
-		},
+        },
+        stock: {
+            type: D.INTEGER,
+            allowNull: false
+        },
+        description: {
+            type: D.STRING,
+            allowNull: false
+        },
+        description_en: {
+            type: D.STRING,
+            allowNull: false
+        },
+        price: {
+            type: D.INTEGER,
+            allowNull: false
+        },
+        score: {
+            type: D.REAL,
+            allowNull: false
+        },
+        sales: {
+            type: D.INTEGER,
+            allowNull: false
+        },
+        is_active: {
+            type: D.BOOLEAN,
+            allowNull: false
+        }
 	});
 };
