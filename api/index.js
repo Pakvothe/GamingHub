4f1877b742858c09b6533c2d4540c062d2c37791
Pakvothe <franqitto@gmail.com>
2020-12-09 10:12:28 -0300

    data contract-v3

--- api/index.js
+++ api/index.js
@@ -1,41 +1,53 @@
-//                   ____
-//                 _.' :  `._
-//             .-.'`.  ;   .'`.-.
-//    __      / : ___\ ;  /___ ; \      __
-//  ,'_ ""--.:__;".-.";: :".-.":__;.--"" _`,
-//  :' `.t""--.. '<@.`;_  ',@>` ..--""j.' `;
-//       `:-.._J '-.-'L__ `-- ' L_..-;'
-//         "-.__ ;  .-"  "-.  : __.-"
-//            L ' /.------.\ ' J
-//              "-.   "--"   .-"
-//             __.l"-:_JL_;-";.__
-//          .-j/'.;  ;""""  / .'\"-.
-//        .' /:`. "-.:     .-" .';  `.
-//     .-"  / ;  "-. "-..-" .-"  :    "-.
-//  .+"-.  : :      "-.__.-"      ;-._   \
-//  ; \  `.; ;                    : : "+. ;
-//  :  ;   ; ;                    : ;  : \:
-// : `."-; ;  ;                  :  ;   ,/;
-//  ;    -: ;  :                ;  : .-"'  :
-//  :\     \  : ;             : \.-"      :
-//   ;`.    \  ; :            ;.'_..--  / ;
-//   :  "-.  "-:  ;          :/."      .'  :
-//     \       .-`.\        /t-""  ":-+.   :
-//      `.  .-"    `l    __/ /`. :  ; ; \  ;
-//        \   .-" .-"-.-"  .' .'j \  /   ;/
-//         \ / .-"   /.     .'.' ;_:'    ;
-//          :-""-.`./-.'     /    `.___.'
-//                \ `t  ._  /
-//                "-.t-._:'
-//     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~             
+//                             `-/osssso+/:.                                    
+//                           `-+hNNmdddhhyysoso+:-`                               
+//                       `osydddhhdddhdhyssyooooooo/-`                            
+//    `              .   oyosyyhhhyhddhyyyhyyy++ooooo+/-`                         
+//    :.`          `..`+yhdsossyyyhhhddydhhhhNy/++yoooooo+-`                      
+//    .--`        `-.`.yMMMNmhhhhyssyyhyhhdddmdyhhdo+oosssso:`                    
+//    `.-.       .--. .ymMMNosdomo+shyddhmNNNddmddooossyyyssso/.                  
+//     `...`````...`  `++yo:-:/-+:/ssssddNMmdmm+::/+ohhhhyyyssoo/`                
+//       `..```..```   `.`` ./+/::/ssydMNmdmms/oshs/+ysymdhhyyssso.               
+//      .....``.....         `./+:oyhNmddNNy::hmdho/sdm//ymmdhyysss:              
+//      ``` ```````             .-//odmNMd/-+hNyyy/+yMNds:/+:oyhhyss/             
+//           ``````            `.:ohyMMMy:-oosdddyosmMNms/os. `.:oyyy-            
+//         `----:--://+so/-:+ohmNNMymMmy/:+hoymNNNydMNyys/+y+     `:os`           
+//       `-/+d+-oshmNNMMMMNMMMMMMMMmhdysossdmmmNmmymNhssy/++o       `.            
+//     .sysdmm+/..:oydmNNNMMNNNNMMMMNNmmmNmmhyhNms++yho+so/+o`                    
+//    .s+smNo:-`     `..--::-..:dmdddMMNmhhyyymmo::+syo+s+oso/                    
+//      /yss-                   ..:hNMNmddhyshMmhhmd+y/os+ssso                    
+//      `                       `+dMMMNmddhysohMMMddhhssoys/so                    
+//                              `-oNNmNNNmdyso+dNMNhssssyyyooo                    
+//                          `.-/oyhmhdhdhhyyy/../MMhysosyyysyo                    
+//                    ``.-/oydmNmddhhyyhyo++/:  `yMmhysoyysys/                    
+//                `./osdmNMNmddmmdhyyhyso+o/:   .mMdmyshsyyy.                    
+//                .+hmNMMMNddmNMmhhhmyhysoss+.    +MNddddhyso                     
+//                  .+hMNdhmMMMNhhdNdhhyssoy+`     sNNNmmNds-                     
+//                 .+dMMdmNMMMMdhdMNydhysso:`      `+mNNNNmo                      
+//                 +MMMMMMMNNmNmdMMmydhyys/          .sNNdo.                      
+//                 `yNMMMMNhshNNNNNdddyso/-            -yd.                       
+//                  `-/dMMNNNNh+++++o/.`                 .                        
+//                     `mMmmds+/+ossoo.                                           
+//                     .mNdhooohNdhdhys:.`                                        
+//                  `.+mddNs:::Nmdhhhhysys.                                       
+//                  +mMMNyho/:+dNMdddddyyy.                                       
+//         ```      `hNmmhysoo+omhNNdmmyso`                                       
+//        ohmds:`  `oNMNhhdho++/. /NNmhoo/                                        
+//       /MMMMMMy.`mMNMNmddyo+/+. oNMMdsso                                        
+//       :NMMMMMMmhmMhmhyhy+.``  `mMMmyyy`                                        
+//        +NMMMMMMMNNddysss`      :hmyyo.                                         
+//         -sNMMMMdhdhhhy/-         ```                                           
+//           `:dMMmyyyy+`                                                         
+//             `/syhs/.                                                      
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~             
 require('dotenv').config();
 const server = require('./src/app.js');
-const { conn } = require('./src/db.js');
-const { PORT } = process.env;
+const {conn} = require('./src/db.js');
+const {PORT} = process.env;
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
-	server.listen(PORT, () => {
-		console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
-	});
+conn.sync({force: true}).then(() => {
+    server.listen(PORT, () => {
+        console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
+    });
 });
