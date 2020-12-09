dd156e8474c1cdb7f639e8f6a97bb5c452a42135
Daniel Tkach <danieltkach@gmail.com>
2020-12-09 12:59:13 -0300

    Added Star Ratings component to product component

--- client/src/components/styles/styled_product.js
+++ client/src/components/styles/styled_product.js
@@ -1,29 +1,53 @@
-import styled from 'styled-components'
+import styled from "styled-components";
+import {COLOR_PRIMARY} from "./../../constants/colors";
 
 export const MainBox = styled.main`
-    max-width: 1200px;
-    width: 80%;
-    margin: 0 auto;
+  max-width: 1200px;
+  width: 80%;
+  margin: 0 auto;
 `;
 
 export const GameDetail = styled.section`
+  display: flex;
+
+  h1 {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 900;
+    height: 100px;
+
+    border: 3px solid blue;
+  }
+
+  .game__img {
+    flex-basis: 200px;
+    flex-grow: 1;
+    margin-right: 30px;
+
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+      object-position: top center;
+      border-radius: 4%;
+      border: 3px solid ${COLOR_PRIMARY};
+    }
+  }
+
+  .game__info {
     display: flex;
+    flex: 2 0;
+    flex-direction: column;
+    margin-left: 30px;
+    justify-content: space-between;
 
-    .game__img{
-        flex-basis: 200px;
-        flex-grow: 1;
-        margin-right: 30px;
+    .game__categories {
+      display: flex;
 
-        img {
-            width: 100%;
-            height: 100%;
-            object-fit: cover;
-            object-position: top center;
-        }
+      justify-content: space-between;
+      list-style-type: none;
     }
 
-    .game__info{
-        flex: 2 0;
-        margin-left: 30px;
+    .game__price {
     }
+  }
 `;
---
+++ client/src/constants/colors.js
@@ -0,0 +1,1 @@
+export const COLOR_PRIMARY = "#9b5df7";
