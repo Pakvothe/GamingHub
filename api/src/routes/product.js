16da106e7a5c3fc543518c5f225129271cd9945c
Daniel Tkach <danieltkach@gmail.com>
2020-12-10 11:40:58 -0300

    Created delete product route, changed language

--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -3,111 +3,129 @@ const { Op } = require('sequelize');
 const { Product, Category } = require('../db.js');
 
 server.get('/', (req, res, next) => {
-    Product.findAll()
-        .then((products) => {
-            res.send(products);
-        })
-        .catch(next);
+	Product.findAll()
+		.then((products) => {
+			res.send(products);
+		})
+		.catch(next);
 });
 
 server.post('/category', (req, res) => {
-    const { name_es, name_en } = req.body
-    Category.create({ name_en, name_es })
-        .then((data) => {
-            res.status(201).json({
-                message: 'OK',
-                data
-            })
-        })
-        .catch((err) => {
-            res.status(400).json({
-                status: 'Bad Request',
-                message: err
-            })
-        })
+	const { name_es, name_en } = req.body
+	Category.create({ name_en, name_es })
+		.then((data) => {
+			res.status(201).json({
+				message: 'OK',
+				data
+			})
+		})
+		.catch(() => {
+			res.status(400).json({
+				message: 'Bad Request'
+			})
+		})
 })
 
 server.get('/category/:catName', (req, res) => {
-    let { catName } = req.params
-    catName = catName.toLowerCase();
-    Product.findAll({
-        include: [{
-            model: Category,
-            where: {
-                [Op.or]: [
-                    { name_en: catName },
-                    { name_es: catName }
-                ]
-            }
-        }]
-    })
-        .then(data => res.json(data))
+	let { catName } = req.params
+	catName = catName.toLowerCase();
+	Product.findAll({
+		include: [{
+			model: Category,
+			where: {
+				[Op.or]: [
+					{ name_en: catName },
+					{ name_es: catName }
+				]
+			}
+		}]
+	})
+		.then(data => res.json(data))
 })
 
 server.put('/category/:catId', (req, res) => {
-    let { catId } = req.params;
-    let { name_es, name_en } = req.body;
-    Category.update({
-        name_es,
-        name_en
-    }, {
-        where: { id: catId },
-        returning: true
-    })
-        .then(data => {
-            data = data[1][0]
-            res.status(200).json({
-                message: "Categoría editada exitosamente",
-                data
-            })
-        })
+	let { catId } = req.params;
+	let { name_es, name_en } = req.body;
+	Category.update({
+		name_es,
+		name_en
+	}, {
+		where: { id: catId },
+		returning: true
+	})
+		.then(data => {
+			data = data[1][0]
+			res.status(200).json({
+				message: "Category edited successfuly.",
+				data
+			})
+		})
 })
 
 server.delete('/category/:catId', (req, res) => {
-    const { catId } = req.params;
-    var category = {};
-    Category.findOne({
-        where: { id: catId }
-    })
-        .then(data => {
-            category = data;
-            return Category.destroy({
-                where: { id: catId }
-            })
-        })
-        .then(() => {
-            res.json(category)
-        })
+	const { catId } = req.params;
+	var category = {};
+	Category.findOne({
+		where: { id: catId }
+	})
+		.then(data => {
+			category = data;
+			return Category.destroy({
+				where: { id: catId }
+			})
+		})
+		.then(() => {
+			res.json(category)
+		})
 })
 
 server.post('/:prodId/category/:catId', (req, res) => {
-    let { prodId, catId } = req.params;
-    let prod = Product.findOne({
-        where: { id: prodId }
-    })
-    let cat = Category.findOne({
-        where: { id: catId }
-    })
-    Promise.all([prod, cat])
-        .then(([prod, cat]) => {
-            prod.addCategories(cat)
-            res.status(201).json({ message: "Producto asociado exitosamente con la categoría" })
-        })
+	let { prodId, catId } = req.params;
+	let prod = Product.findOne({
+		where: { id: prodId }
+	})
+	let cat = Category.findOne({
+		where: { id: catId }
+	})
+	Promise.all([prod, cat])
+		.then(([prod, cat]) => {
+			prod.addCategories(cat)
+			res.status(201).json({ message: "Product associated with category successfully." })
+		})
 })
 
 server.delete('/:prodId/category/:catId', (req, res) => {
-    let { prodId, catId } = req.params;
-    let prod = Product.findOne({
-        where: { id: prodId }
-    })
-    let cat = Category.findOne({
-        where: { id: catId }
-    })
-    Promise.all([prod, cat])
-        .then(([prod, cat]) => {
-            prod.removeCategories(cat)
-            res.status(200).json({ message: "Categoría eliminada" })
-        })
+	let { prodId, catId } = req.params;
+	let prod = Product.findOne({
+		where: { id: prodId }
+	})
+	let cat = Category.findOne({
+		where: { id: catId }
+	})
+	Promise.all([prod, cat])
+		.then(([prod, cat]) => {
+			prod.removeCategories(cat)
+			res.status(200).json({ message: "Category deleted." })
+		})
+})
+
+server.delete('/:id', (req, res) => {
+	const prodId = req.params.id;
+	Product.destroy({
+		where: {
+			id: prodId
+		}
+	}).then(data => {
+		if (!data) {
+			res.status(404).json({ message: 'Product not found.' })
+		} else {
+			res.status(200).json({ message: 'Product deleted.' })
+		}
+	}).catch(() => {
+		res.status(500).json({
+			message: 'Internal server error',
+		})
+	});
 })
 
 module.exports = server;
