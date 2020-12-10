039eb82848620db281b26512c49fdfa47ef2a030
Pakvothe <franqitto@gmail.com>
2020-12-10 17:10:34 -0300

    admin product page - v1.2

--- client/src/components/admin_product_page/admin_product_container/index.js
+++ client/src/components/admin_product_page/admin_product_container/index.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
-import { connect } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import ProductForm from '../admin_product_form/';
-import SearchBar from '../admin_search_bar/'
+import SearchBar from '../admin_search_bar/';
 
-const AdminProductContainer = () => {
+const AdminProductContainer = (p) => {
+	const dispatch = useDispatch();
+	const products = useSelector((state) => state.products)
 	const [state, setState] = useState({
 		show_form: false,
 		show_products: false
@@ -15,14 +17,14 @@ const AdminProductContainer = () => {
 			show_products: false
 		}
 		)
-	}
+	};
 
 	const handleProduct = () => {
 		setState({
 			show_form: false,
 			show_products: true
 		})
-	}
+	};
 
 	const showForm = state.show_form;
 	const showProducts = state.show_products;
@@ -34,10 +36,35 @@ const AdminProductContainer = () => {
 				<button onClick={handleForm}>Agregar Producto</button>
 			</div>
 			{
-				showForm ? (<ProductForm />) : showProducts ? (<h1>list</h1>) : null
+				showForm ? (<ProductForm />) : showProducts ? (<p>{p.products[0].name}</p>) : null
 			}
 		</div>
-	)
+	);
+};
+
+AdminProductContainer.defaultProps = {
+	products: [
+		{
+			name: 'Final Fantasy VII Remake',
+			stock: 100,
+			description_es: 'El mundo ha caído bajo el control de Shinra Electric Power Company, una oscura corporación que controla la fuerza vital del planeta como energía mako. En la extensa ciudad de Midgar, una organización anti-Shinra que se hace llamar Avalanche ha intensificado su resistencia. Cloud Strife, un ex miembro de la unidad SOLDADO de élite de Shinra ahora convertido en mercenario, presta su ayuda al grupo, sin darse cuenta de las consecuencias épicas que le esperan',
+			description_en: 'The world has fallen under the control of the Shinra Electric Power Company, a shadowy corporation controlling the planet very life force as mako energy. In the sprawling city of Midgar, an anti-Shinra organization calling themselves Avalanche have stepped up their resistance. Cloud Strife, a former member of Shinra is elite SOLDIER unit now turned mercenary, lends his aid to the group, unaware of the epic consequences that await him.',
+			price: 52.38,
+			score: 5,
+			sales: 370,
+			is_active: true
+		},
+		{
+			name: 'FIFA 21',
+			stock: 200,
+			description_es: 'Los desarrolladores del juego han tenido años para alcanzar la perfección y, como resultado, hay poco en lo que se ha interferido para arruinar el clásico tan querido. Como siempre, es la licencia lo que atrae a los compradores, y esta edición no es diferente con la asombrosa cantidad de 17,000 jugadores de 700 clubes que juegan en 30 ligas para elegir, intercambiar y transferir. Esto significa que puedes jugar los partidos de una temporada completa sin repetir a un jugador, incluso en el mismo equipo, si así lo deseas.',
+			description_en: 'The game developers have had years to reach perfection and as a result there is little that has been interfered with to ruin the well-loved classic. As always, it is the licensing that attracts buyers, and this edition is no different with a staggering 17,000 players from 700 clubs playing in 30 leagues to choose from, swap and transfer and change the fortunes of! This means you could play out an entire season’s worth of games without repeating a player, even in the same team, if you wanted to! ',
+			price: 40.72,
+			score: 4.3,
+			sales: 432,
+			is_active: true
+		}
+	]
 }
 
 export default AdminProductContainer;
--- client/src/components/admin_product_page/admin_product_form/index.js
+++ client/src/components/admin_product_page/admin_product_form/index.js
@@ -1,33 +1,33 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const AdminProductForm = (p) => {
+const AdminProductForm = () => {
 	return (
 		<form>
 			<label>
-				nombre:
+				Nombre:
 				<input type='text' name='product_name' />
 			</label>
 			<label>
-				stock:
+				Stock:
 				<input type='text' name='product_stock' />
 			</label>
 			<label>
-				description:
+				Description:
 				<textarea type='text' name='product_description'>
 				</textarea>
+			</label>
+			<label>
+				Price:
+				<input type='text' name='product_price' />
+			</label>
+			<label>
+				Activo:
+				<input type='checkbox' name='product_is_active' />
 			</label>
 
 		</form>
 	);
 };
 
-AdminProductForm.defaultProps = {
-	name: "Final Fantasy VII Remake",
-	stock: 100,
-	description: {
-		es:
-			"El mundo ha caído bajo el control de Shinra Electric Power Company, una oscura corporación que controla la fuerza vital del planeta como energía mako. En la extensa ciudad de Midgar, una organización anti-Shinra que se hace llamar Avalanche ha intensificado su resistencia. Cloud Strife, un ex miembro de la unidad SOLDADO de élite de Shinra ahora convertido en mercenario, presta su ayuda al grupo, sin darse cuenta de las consecuencias épicas que le esperan",
-	}
-}
 export default AdminProductForm;
