dcb1455b09f12c9f5c2eff786cf035e21312d3fa
Ignacio Contreras <nc.devw@gmail.com>
2020-12-12 19:40:02 -0300

    deleted comments

--- client/src/components/admin_page/admin_product_form/index.js
+++ client/src/components/admin_page/admin_product_form/index.js
@@ -91,49 +91,4 @@ const AdminProductForm = ({ categories }) => {
 	);
 };
 
-// AdminProductForm.defaultProps = {
-// 	categories: [
-// 		{
-// 			name_en: 'fps',
-// 			name_es: 'fps',
-// 		},
-// 		{
-// 			name_en: 'action',
-// 			name_es: 'accion',
-// 		},
-// 		{
-// 			name_en: 'simulation',
-// 			name_es: 'simulacion',
-// 		},
-// 		{
-// 			name_en: 'sports',
-// 			name_es: 'deportes',
-// 		},
-// 		{
-// 			name_en: 'rpg',
-// 			name_es: 'rpg',
-// 		},
-// 		{
-// 			name_en: 'strategy',
-// 			name_es: 'estrategia',
-// 		},
-// 		{
-// 			name_en: 'platformer',
-// 			name_es: 'plataforma',
-// 		},
-// 		{
-// 			name_en: 'adventure',
-// 			name_es: 'aventura',
-// 		},
-// 		{
-// 			name_en: 'arcade',
-// 			name_es: 'arcade',
-// 		},
-// 		{
-// 			name_en: 'racing',
-// 			name_es: 'carreras',
-// 		},
-// 	]
-// }
-
 export default AdminProductForm;
