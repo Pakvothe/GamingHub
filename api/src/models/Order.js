02feb341f79abf9d09c7009a44fae86da422a1a8
Pakvothe <franqitto@gmail.com>
2020-12-14 14:30:43 -0300

    complete order model in db - v1

---
+++ api/src/models/Order.js
@@ -0,0 +1,34 @@
+const { DataTypes } = require('sequelize');
+const D = DataTypes;
+
+module.exports = (sequelize) => {
+	sequelize.define('order', {
+		email: {
+			type: D.STRING,
+			unique: true,
+			allowNull: false,
+			validate: {
+				isEmail: true
+			}
+		},
+		total_amount: {
+			type: D.REAL,
+			unique: true,
+			allowNull: false,
+			validate: {
+				isDecimal: true
+			}
+		},
+
+		state: {
+			type: D.ENUM('created', 'processing', 'completed', 'canceled'),
+			unique: true,
+			allowNull: false
+		},
+		payment_method: {
+			type: D.ENUM('cash', 'cc', 'mp'),
+			unique: true,
+			allowNull: false
+		}
+	})
+}
