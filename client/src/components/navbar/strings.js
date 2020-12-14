ba454f6edd9c0d916bfe82d2c1d89dc637cfbd66
Ignacio Contreras <nc.devw@gmail.com>
2020-12-14 16:21:38 -0300

    Added button language and more links

---
+++ client/src/components/navbar/strings.js
@@ -0,0 +1,24 @@
+const strings = {
+	en: {
+		language: 'Language',
+		language_en: "English",
+		language_es: "Spanish",
+		user: 'User',
+		cart: 'Cart',
+		categories: 'Categories',
+		offer: 'Offer',
+		about: 'About'
+	},
+	es: {
+		language: 'Idioma',
+		language_en: "Inglés",
+		language_es: "Español",
+		user: 'Usuario',
+		cart: 'Carrito',
+		categories: 'Categorias',
+		offer: 'Ofertas',
+		about: 'Conocenos'
+	}
+};
+
+export default strings;
