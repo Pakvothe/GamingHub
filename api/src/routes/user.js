7b83e873d405939e494eaeb48636c7d4f59fb635
Pakvothe <franqitto@gmail.com>
2020-12-15 13:21:59 -0300

    complete delete user route - v1

--- api/src/routes/user.js
+++ api/src/routes/user.js
@@ -13,8 +13,37 @@ server.get('/', (req, res) => {
 			res.status(200).send(users);
 		})
 		.catch((err) => {
-			res.status(500).json({ message: "Internal server error" });
+			res.status(500).json({
+				message: "Internal server error"
+			});
 		})
 });
 
+server.delete('/:userId', (req, res) => {
+	const { userId } = req.params;
+	var user = {};
+
+	User.findOne({
+		where: {
+			id: userId
+		}
+	})
+		.then(data => {
+			user = data;
+			return User.destroy({
+				where: {
+					id: userId
+				}
+			})
+		})
+		.then((data) => {
+			if (data === 0) {
+				res.status(404).json({ message: "Bad request" })
+			}
+			res.json(user)
+		})
+		.catch((err) => {
+			res.status(500).json({ message: "Internal server error" });
+		})
+})
 module.exports = server; 
