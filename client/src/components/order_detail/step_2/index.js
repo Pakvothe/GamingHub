79eee35d41a53af57448748bbf4814591768c5a6
Pakvothe <franqitto@gmail.com>
2020-12-22 22:11:36 -0300

    more bugfix

--- client/src/components/order_detail/step_2/index.js
+++ client/src/components/order_detail/step_2/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
-import { useHistory } from 'react-router-dom';
+import { Redirect, useHistory } from 'react-router-dom';
 import CreditCard from '../step_2/CreditCard';
 import { addCreditCard } from '../../../redux/actions/global_actions';
 //------>Styles
 import { StyledSVG, StepTwo } from '../../styles/styled_order_detail';
 import PurchaseStep2 from '../../../assets/img/purchase-steps-2.svg';
 
-const Step2 = () => {
+const Step2 = ({ cart }) => {
 	const dispatch = useDispatch();
 	const history = useHistory();
 
@@ -16,6 +16,9 @@ const Step2 = () => {
 		history.push('/order/detail')
 	}
 
+	if (cart.length < 1) {
+		return <Redirect to='/' />
+	}
 	return (
 		<>
 			<h2>Datos de pago</h2>
