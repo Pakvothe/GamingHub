73bef922724ff24780e25fc72bb4e9265b246cd1
Pakvothe <franqitto@gmail.com>
2021-01-08 17:25:23 -0300

    Add alert for toogleAdmin in admin user panel, and strings

--- client/src/components/admin_page/admin_user_list/index.js
+++ client/src/components/admin_page/admin_user_list/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { deleteUser, toggleAdmin } from '../../../redux/actions/users_actions';
 import { DataTable } from '../../styles/styled_global';
+import Swal from 'sweetalert2';
+import strings from './strings'
 
 const AdminUserList = ({ users }) => {
 	const dispatch = useDispatch();
+	const language = useSelector(state => state.globalReducer.language);
 
 	const handleDelete = (id) => {
 		dispatch(deleteUser(id));
@@ -12,7 +15,25 @@ const AdminUserList = ({ users }) => {
 	}
 
 	const handleInput = (id, is_admin) => {
-		dispatch(toggleAdmin({ id, is_admin }))
+		Swal.fire({
+			heightAuto: false,
+			title: strings[language].admin_confirm,
+			text: strings[language].admin_confirm_text,
+			icon: 'warning',
+			showCancelButton: true,
+			confirmButtonColor: '#3085d6',
+			cancelButtonColor: '#d33',
+			confirmButtonText: strings[language].admin_button,
+		}).then((result) => {
+			if (result.isConfirmed) {
+				Swal.fire(
+					strings[language].admin_confirm_2,
+					strings[language].admin_confirm_text2,
+					'success',
+					dispatch(toggleAdmin({ id, is_admin }))
+				)
+			}
+		})
 	}
 
 	return (
---
+++ client/src/components/admin_page/admin_user_list/strings.js
@@ -0,0 +1,22 @@
+const strings = {
+	en: {
+
+		admin_confirm: 'Change permissions!',
+		admin_confirm_2: 'Success',
+		admin_confirm_text: 'Are you sure?',
+		admin_button: 'Yes!',
+		admin_confirm_text2: 'User permissions successfully changed',
+
+	},
+	es: {
+
+		admin_confirm: 'Cambiar permisos!',
+		admin_confirm_2: 'Exito',
+		admin_confirm_text: '¿Estás Seguro?',
+		admin_button: 'Si!',
+		admin_confirm_text2: 'Permisos de usuario cambiados correctamente',
+
+	}
+};
+
+export default strings;
