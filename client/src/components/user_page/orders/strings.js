f7f42b8257ae4abcc09cece9460ae665c301970a
Pakvothe <franqitto@gmail.com>
2021-01-08 17:37:26 -0300

    Add language strings in user orders

---
+++ client/src/components/user_page/orders/strings.js
@@ -0,0 +1,20 @@
+const strings = {
+	en: {
+		title: "Orders",
+		id: "Nº Order",
+		mail: "Email",
+		total: "Total",
+		state: "State",
+		payment: "Payment Method",
+	},
+	es: {
+		title: "Órdenes",
+		id: "Nº Orden",
+		mail: "Email",
+		total: "Total",
+		state: "Estado",
+		payment: "Metodo de Pago",
+	}
+};
+
+export default strings;
