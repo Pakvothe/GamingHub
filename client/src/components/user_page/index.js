16c4c090c56161c30ac829a9f5490cb9aac9dd27
Pakvothe <franqitto@gmail.com>
2021-01-08 17:31:24 -0300

    Add language strings in profile

--- client/src/components/user_page/index.js
+++ client/src/components/user_page/index.js
@@ -6,11 +6,15 @@ import Profile from './profile'
 
 /* --- Styled --- */
 import { UserCard } from '../styles/styled_user_card'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { getUser } from '../../redux/actions/users_actions'
+
+/* --- Strings --- */
+import strings from './strings';
 
 const UserPage = () => {
 	const dispatch = useDispatch()
+	const language = useSelector(state => state.globalReducer.language)
 
 	useEffect(() => {
 		dispatch(getUser());
@@ -19,9 +23,9 @@ const UserPage = () => {
 	return (
 		<UserCard>
 			<ul>
-				<li><Link to="/edit">Editar Perfil</Link></li>
-				<li><Link to="/orders">Mis Compras</Link></li>
-				<li><Link to="/">Ayuda</Link></li>
+				<li><Link to="/edit">{strings[language].edit} </Link></li>
+				<li><Link to="/orders">{strings[language].orders} </Link></li>
+				<li><Link to="/">{strings[language].help} </Link></li>
 			</ul>
 			<Profile />
 		</UserCard>
