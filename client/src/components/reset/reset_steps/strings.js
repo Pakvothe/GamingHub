c8a4b284a50c28930f1742f0803acc2fa3666382
tinsanchez00 <31607514+tinsanchez00@users.noreply.github.com>
2021-01-08 03:06:02 +0100

    Create reset component

---
+++ client/src/components/reset/reset_steps/strings.js
@@ -0,0 +1,36 @@
+export default {
+	en: {
+		description: {
+			1: 'MISSING',
+			2: 'MISSING',
+			3: 'MISSING'
+		},
+		label: {
+			1: 'MISSING',
+			2: 'MISSING',
+			3: 'MISSING'
+		},
+		button: {
+			1: 'MISSING',
+			2: 'MISSING',
+			3: 'MISSING'
+		}
+	},
+	es: {
+		description: {
+			1: 'Ingresa tu correo y te enviaremos un codigo para restablecer tu contraseña',
+			2: 'Ingresa el codigo que hemos enviado a tu correo para restablecer tu contraseña',
+			3: 'Ingresa tu nueva contraseña'
+		},
+		label: {
+			1: 'Correo',
+			2: 'Codigo',
+			3: 'Nueva Contraseña'
+		},
+		button: {
+			1: 'Enviar Codigo',
+			2: 'Comprobar Codigo',
+			3: 'Cambiar Contraseña'
+		}
+	}
+}
