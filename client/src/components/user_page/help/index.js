dc40a3452244aed9a0429329f29eb5b8a1b3abc6
Pakvothe <franqitto@gmail.com>
2021-01-10 22:10:30 -0300

    Add Fade in help component

--- client/src/components/user_page/help/index.js
+++ client/src/components/user_page/help/index.js
@@ -3,19 +3,20 @@ import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { ProfileHelp } from '../../styles/styled_profile';
 import strings from './strings';
+import Fade from 'react-reveal/Fade';
 
 const HelpUser = () => {
 	const language = useSelector((state) => state.globalReducer.language);
 	const s = strings[language];
 	return (
-		<>
+		<Fade>
 			<ProfileHelp>
 				<h2>{s.title}</h2>
 				<p>{s.text}</p>
 				<a href='mailto:soygaminghub@gmail.com?Subject=User%20Help'>soygaminghub@gmail.com</a>
 				<Link className='back' to='/user'>{s.volver}</Link>
 			</ProfileHelp>
-		</>
+		</Fade>
 	)
 }
 
