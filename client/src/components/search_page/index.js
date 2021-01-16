a07f63e694ce8d35594b3b90f449920a4b9a3886
franfiori29 <franfiori@gmail.com>
2021-01-16 16:31:23 -0300

    scuffed

--- client/src/components/search_page/index.js
+++ client/src/components/search_page/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { changeCurrentPage } from '../../redux/actions/global_actions';
 import { getSearchProducts } from '../../redux/actions/products_actions';
@@ -9,6 +9,7 @@ const SearchPage = () => {
 	const dispatch = useDispatch()
 
 	const { productList: products, isLoading, error } = useSelector(state => state.productsReducer.productsFilter);
+	const totalProducts = useSelector(state => state.productsReducer.products.productList);
 	const language = useSelector(state => state.globalReducer.language);
 
 	const limitPerPage = 8;
@@ -17,18 +18,21 @@ const SearchPage = () => {
 	const queryparams = new URLSearchParams(querystring)
 
 	const handlePageChange = (ev) => {
-		const offset = limitPerPage * ev.selected
-		dispatch(changeCurrentPage(ev.selected));
+		const offset = ev && ev?.selected !== 0 ? limitPerPage * ev.selected : 0;
+		ev && dispatch(changeCurrentPage(ev.selected));
 		dispatch(getSearchProducts(queryparams.get('query'), { limit: limitPerPage, offset }))
 	}
+
+	useEffect(handlePageChange, [totalProducts]);
+
 	return (
 		<div>
-			<Catalog products={products.filter(pr => pr.is_active)}
+			{products && <Catalog products={products.filter(pr => pr.is_active)}
 				isLoading={isLoading}
 				error={error}
 				language={language}
 				handlePageChange={handlePageChange}
-			/>
+			/>}
 		</div>
 	)
 }
